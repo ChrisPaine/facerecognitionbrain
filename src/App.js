cac65f3ceeb54a13a3a21f004d044f20e514422b
Chris Paine <cdpaine2003@gmail.com>
2020-08-07 10:46:09 -0400

    init push my app

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://tranquil-cliffs-96727.herokuapp.com/imageurl', {
         method: 'post',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
@@ -89,7 +89,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://tranquil-cliffs-96727.herokuapp.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
